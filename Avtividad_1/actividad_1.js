aed751cb3d71adce6628997f3eebd7efb090e4b3
NolberthB <betanolberto14@gmail.com>
2022-04-26 21:33:56 -0500

    Actividad1

---
+++ Avtividad_1/actividad_1.js
@@ -0,0 +1,21 @@
+
+//  EJERCICIO 1
+var num1 = Number(prompt('Escribe un número: '))
+alert(num1 * 2)
+
+
+// EJERCICIO 2
+var num1 = Number(prompt('Escribe el primer número: '))
+var num2 = Number(prompt('Escribe el segundo número: '))
+alert(num1 - num2)
+
+// EJERCICIO 3
+var r = 5.3 
+var radio = (Math.PI * Math.pow(r,2))
+alert('El radio es:' + radio)
+
+// EJERCICIO 4
+var D = 8
+var d = 6
+var a = (D * d)/2 
+alert('El área del rombo es: ' + a)
